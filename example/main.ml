d0d38e6a05f45d22d6e6086df6208efa6657953d
Anton Bachin <antonbachin@yahoo.com>
2022-02-18 17:15:01 +0300

    Tweak example code
    - Delete environment variables $DEBUG and $INTERFACE.
    - Avoid abbreviations like "req" (unnecessary mental load compared to
      just using the original words, which are not much longer).
    - Replace <uk> by </ul>.
    - Unwrap the values that were inside the Message module.

--- example/main.ml
+++ example/main.ml
@@ -2,44 +2,37 @@ let client_id = Sys.getenv "OAUTH2_CLIENT_ID"
 let client_secret = Sys.getenv "OAUTH2_CLIENT_SECRET"
 let redirect_uri = Sys.getenv "OAUTH2_REDIRECT_URI"
 
-module Messages = struct
-  type message = {
-    user : string;
-    message : string;
-  }
+type message = {
+  user : string;
+  message : string;
+}
 
-  let messages = ref [{ user = "Unknown"; message = "HELLO" }]
+let messages = ref [{ user = "Unknown"; message = "HELLO" }]
 
-  let render () =
-    let items =
-      !messages
-      |> ListLabels.map ~f:(fun msg ->
-             Printf.sprintf "<li>%s: %s</li>" msg.user msg.message)
-      |> String.concat "\n"
-    in
-    Printf.sprintf "<ul>%s<uk>" items
+let render_messages () =
+  let items =
+    !messages
+    |> ListLabels.map ~f:(fun msg ->
+           Printf.sprintf "<li>%s: %s</li>" msg.user msg.message)
+    |> String.concat "\n"
+  in
+  Printf.sprintf "<ul>%s</ul>" items
 
-  let add ~user message = messages := { user; message } :: !messages
-end
+let add_message ~user message = messages := { user; message } :: !messages
 
 let () =
-  let () =
-    match Sys.getenv_opt "DEBUG" with
-    | None | Some ("no" | "NO" | "0") -> ()
-    | Some _ -> Dream.initialize_log ~level:`Debug ()
-  in
-  Dream.run ~adjust_terminal:false ?interface:(Sys.getenv_opt "INTERFACE")
+  Dream.run
   @@ Dream.logger
   @@ Dream.memory_sessions
   @@ Dream.router
        [
          Dream_oauth2.route ~client_id ~client_secret ~redirect_uri ();
-         Dream.get "/" (fun req ->
-             match Dream_oauth2.user_profile req with
+         Dream.get "/" (fun request ->
+             match Dream_oauth2.user_profile request with
              | None ->
                Dream.respond
                  ("<p>Please <a href='/oauth2/signin'>sign in</a>!"
-                 ^ Messages.render ())
+                 ^ render_messages ())
              | Some user ->
                let header =
                  Printf.sprintf
@@ -52,16 +45,16 @@ let () =
                         <input type="submit" />
                       </form>
                     |}
-                   user.Dream_oauth2.User_profile.user (Dream.csrf_tag req)
+                   user.Dream_oauth2.User_profile.user (Dream.csrf_tag request)
                in
-               Dream.respond (header ^ Messages.render ()));
-         Dream.post "/" (fun req ->
-             match Dream_oauth2.user_profile req with
-             | None -> Dream.redirect req "/"
+               Dream.respond (header ^ render_messages ()));
+         Dream.post "/" (fun request ->
+             match Dream_oauth2.user_profile request with
+             | None -> Dream.redirect request "/"
              | Some user -> (
-               match%lwt Dream.form req with
+               match%lwt Dream.form request with
                | `Ok [("message", message)] ->
-                 Messages.add ~user:user.Dream_oauth2.User_profile.user message;
-                 Dream.redirect req "/"
-               | _ -> Dream.redirect req "/"));
+                 add_message ~user:user.Dream_oauth2.User_profile.user message;
+                 Dream.redirect request "/"
+               | _ -> Dream.redirect request "/"));
        ]
