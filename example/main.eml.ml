a6ac4f6cfc7491b5f55a779d2866888db0f0f570
Anton Bachin <antonbachin@yahoo.com>
2022-02-21 13:36:39 +0300

    Simplify example application a bit

--- example/main.eml.ml
+++ example/main.eml.ml
@@ -7,32 +7,43 @@ type message = {
   text : string;
 }
 
-let messages = ref [{ user = "Unknown"; text = "HELLO" }]
-
-let add_message ~user text = messages := { user; text } :: !messages
+let messages = ref [
+  {user = "admin"; text = "This is a dummy message of the day!"};
+]
 
 let render request =
   <html>
+  <head>
+  <style>
+
+  body, input {
+    font-family: Helvetica, sans-serif;
+    font-size: 24px;
+  }
+
+  </style>
+  </head>
   <body>
 
 % begin match Dream_oauth2.user_profile request with
 % | None ->
-    <p>Please <a href='/oauth2/signin'>sign in</a>!
+    <p>Please sign in to chat!</p>
+    <p><a href="/oauth2/signin">Sign in with GitHub</a></p>
+    <hr>
 % | Some profile ->
-    <p>Hello, <%s profile.Dream_oauth2.User_profile.user %>!<p>
-    <p><a href='/oauth2/signout'>Sign out</a></p>
+    <p>Signed in as <%s profile.Dream_oauth2.User_profile.user %>.<p>
+    <p><a href="/oauth2/signout">Sign out</a></p>
+    <hr>
     <form method="POST" action="/">
       <%s! Dream.csrf_tag request %>
-      <textarea name="message"></textarea>
-      <input type="submit" />
+      <input type="text" name="text" autofocus>
+      <input type="submit" value="Send">
     </form>
 % end;
 
-  <ul>
-%    !messages |> List.iter begin fun message ->
-      <li><%s message.user %>: <%s message.text %></li>
-%   end;
-  </ul>
+% !messages |> List.iter begin fun message ->
+    <p><b><%s message.user %></b>&nbsp;&nbsp;&nbsp;<%s message.text %></p>
+% end;
 
   </body>
   </html>
@@ -41,18 +52,23 @@ let () =
   Dream.run
   @@ Dream.logger
   @@ Dream.memory_sessions
-  @@ Dream.router
-       [
-         Dream_oauth2.route ~client_id ~client_secret ~redirect_uri ();
-         Dream.get "/" (fun request ->
-          Dream.html (render request));
-         Dream.post "/" (fun request ->
-             match Dream_oauth2.user_profile request with
-             | None -> Dream.redirect request "/"
-             | Some user -> (
-               match%lwt Dream.form request with
-               | `Ok [("message", message)] ->
-                 add_message ~user:user.Dream_oauth2.User_profile.user message;
-                 Dream.redirect request "/"
-               | _ -> Dream.redirect request "/"));
-       ]
+  @@ Dream.router [
+
+    Dream_oauth2.route ~client_id ~client_secret ~redirect_uri ();
+
+    Dream.get "/" (fun request ->
+      Dream.html (render request));
+
+    Dream.post "/" (fun request ->
+      match Dream_oauth2.user_profile request with
+      | None ->
+        Dream.redirect request "/"
+      | Some profile ->
+        match%lwt Dream.form request with
+        | `Ok ["text", text] ->
+          messages :=
+            {user = profile.Dream_oauth2.User_profile.user; text}::!messages;
+          Dream.redirect request "/"
+        | _ ->
+          Dream.redirect request "/");
+  ]
