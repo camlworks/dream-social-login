c0c0f5b49417872d15498399366bf75fbd693d0f
Andrey Popp <8mayday@gmail.com>
2022-03-03 20:12:43 +0300

    Expose Internal.Hyper_helper

--- oauth2/dream_oauth2.ml
+++ oauth2/dream_oauth2.ml
@@ -4,7 +4,12 @@ type authenticate_result = Oauth2.authenticate_result
 and provider_error = Oauth2.provider_error
 
 let provider_error_to_string = Oauth2.provider_error_to_string
+let provider_error_of_string = Oauth2.provider_error_of_string
 
 module Github = Github
 module Twitch = Twitch
 module Stackoverflow = Stackoverflow
+
+module Internal = struct
+  module Hyper_helper = Hyper_helper
+end
