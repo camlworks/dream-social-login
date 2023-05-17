5094afcafaba837093226d24568f3fbd458c0c31
Anton Bachin <antonbachin@yahoo.com>
2023-05-17 20:45:39 +0300

    Finish renaming in previous commit

--- src/oauth/dream_oauth.ml
+++ src/oauth/dream_oauth.ml
@@ -1,10 +1,10 @@
-module User_profile = Oauth2.User_profile
+module User_profile = Oauth.User_profile
 
-type authenticate_result = Oauth2.authenticate_result
-and provider_error = Oauth2.provider_error
+type authenticate_result = Oauth.authenticate_result
+and provider_error = Oauth.provider_error
 
-let provider_error_to_string = Oauth2.provider_error_to_string
-let provider_error_of_string = Oauth2.provider_error_of_string
+let provider_error_to_string = Oauth.provider_error_to_string
+let provider_error_of_string = Oauth.provider_error_of_string
 
 module Github = Github
 module Twitch = Twitch
